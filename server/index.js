ed722a9a32a9d96cd5e50ac96f6cdf43d5ddc864
kfranklin <kfranklin93@gmail.com>
2016-05-31 10:53:00 -0400

    Added MovieController.js

--- server/index.js
+++ server/index.js
@@ -23,6 +23,10 @@ app.use(function(req, res, next){
 //Connect to MongoDB
 mongoose.connect('mongodb://localhost/movieapp');
 mongoose.connection.once('open', function(){
+
+    //Load the models.
+app.models = require('./models/index');
+
 console.log('Listening on port 3000 ...');
 app.listen(3000);
 });
